9213a85a3b9cf561f7e45287256cff3fbe6f0996
Bairon1220 <bairon122085@hotmail.com>
2020-12-29 11:12:42 -0500

    Ajustes

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,16 @@
 ##A continuación se muestran un par de funciones que almacenan en caché la inversa de una matrix siempre qwue sea reversible
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ##Se iicia el NUll
   inv <- NULL
+  
+  ##Se selecciona la matriz
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
   get <- function() x
+  ##Aqui es donde se invierte la matrix
   setInverse <- function(inverse) inv <<- inverse
   getInverse <- function() inv
   list(set = set,
@@ -15,15 +20,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Aqui la solucion
+## Aqui la solucion donde se calcula la matriz inversa
 
 cacheSolve <- function(x, ...) {
   inv  <-  x $ getInverse ()
   if ( ! is.null ( inv )) {
     volver ( inv )
   }
+  ##Aqui se obtiene la matriz inversa
   mat  <-  x $ obtener ()
   inv  <- resolver ( mat , ... )
+  ##Se seleccina la matriz
   x $ setInverse ( inv )
+  ##Retorna la matriz
   inv
   }
